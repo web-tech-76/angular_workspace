280e4a558506a4fa37b0fe2dd37a317fabc8fc58
web-tech-76 <webtec76@gmail.com>
2023-12-07 07:39:13 -0500

    made changes to app

--- new-web-ui/src/app/app.component.ts
+++ new-web-ui/src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLinkActive, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'new-web-ui';
+  title = 'New Web UI';
+
+
+  constructor() {
+
+  }
+
+
 }
