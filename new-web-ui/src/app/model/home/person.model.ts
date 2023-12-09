7ebe9fcde0a288b0e0e86c63240303797509217d
web-tech-76 <webtec76@gmail.com>
2023-12-09 12:55:02 -0500

    new proj

--- new-web-ui/src/app/model/home/person.model.ts
+++
@@ -1,7 +0,0 @@
-
-
-export class PersonModel {
-
-  constructor(public id: number , public name: string) {
-  }
-};
--- new-web-ui/src/app/model/login/user.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface UserInterface {
-  username: string;
-  token: string;
-  email: string;
-
-}
